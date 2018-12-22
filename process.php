99b26d5c9ee4b33a0708a952ab7022594e5c1509
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-22 23:18:23 +0300

    payment1

--- process.php
+++ process.php
@@ -1,26 +1,26 @@
 <?php
-
-if (!empty($_POST)){
-    die;
-}
-
-require_once __DIR__.'/inc/db.php';
-$key ='';
-$key_id = '';
-$dataSet = $_POST;
-
-unset($dataSet['ik_sign']);
-ksort($dataSet, SORT_STRING);
-array_push($dataSet,$key);
-$signString = implode(':',$dataSet);
-$sign = base64_encode(md5($signString, true));
-
-$order = R::load ('orders',(int)$dataSet['ik_pm_no']);
-if (!$order) die;
-
-if ($dataSet['ik_co_id'] != $ik_id||$dataSet['ik_inv_st'] != 'success'||
-$dataSet['ik_am']!=$order->price||$sign!=$_POST['ik_sign']){
-    die;
-}
-$order->status ='1';
-R::store($order);
+//
+//if (!empty($_POST)){
+//    die;
+//}
+//
+//require_once __DIR__.'/inc/db.php';
+//$key ='';
+//$key_id = '';
+//$dataSet = $_POST;
+//
+//unset($dataSet['ik_sign']);
+//ksort($dataSet, SORT_STRING);
+//array_push($dataSet,$key);
+//$signString = implode(':',$dataSet);
+//$sign = base64_encode(md5($signString, true));
+//
+//$order = R::load ('orders',(int)$dataSet['ik_pm_no']);
+//if (!$order) die;
+//
+//if ($dataSet['ik_co_id'] != $ik_id||$dataSet['ik_inv_st'] != 'success'||
+//$dataSet['ik_am']!=$order->price||$sign!=$_POST['ik_sign']){
+//    die;
+//}
+//$order->status ='1';
+//R::store($order);
