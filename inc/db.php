c4e1469c84df0c979282121fcbb87212d9d96dee
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-23 01:59:47 +0300

    postgres

--- inc/db.php
+++ inc/db.php
@@ -2,9 +2,12 @@
 
 require_once 'rb.php';
 
-$db = array(
-   'dsn' => 'mysql:host=localhost; dbname=payment; charset=utf8',
-    'user' =>'root',
-    'pass'=>''
-);
+$db = parse_url(getenv("DATABASE_URL"));
+$db["path"] = ltrim($db["path"], "/");
+
+//$db = array(
+//   'dsn' => 'mysql:host=localhost; dbname=payment; charset=utf8',
+//    'user' =>'root',
+//    'pass'=>''
+//);
 R::setup($db['dsn'], $db['user'],$db['pass']);
