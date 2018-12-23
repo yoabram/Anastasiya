c0be6d4004e2aa2a3fe333c70117625016a514f6
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-23 22:05:30 +0300

    pg_close

--- inc/functions.php
+++ inc/functions.php
@@ -29,6 +29,7 @@ function load ($data){
 function save ($db, $data){
     $query ="INSERT INTO orders (\"name\", \"fam\", \"number\", \"email\", \"product\", \"price\") VALUES ($data)";
     pg_insert($db, 'orders',$data );
+    pg_close($db);
 //    $tbl = R::dispense($table);
 //    foreach ($data as $k => $v){
 //        $tbl->$k = $v;
