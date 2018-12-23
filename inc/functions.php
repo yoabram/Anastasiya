1231b03553736bf52fafd337e685030b0443027e
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-23 19:17:26 +0300

    12

--- inc/functions.php
+++ inc/functions.php
@@ -6,8 +6,7 @@ if(!empty($_POST)){
     $data = load($data);
     $order_id = save('orders', $data);
     setPaymentData($order_id);
-    //header('Location: inc/form.php');
-    debug($data);
+    header('Location: inc/form.php');
 }
 function setPaymentData($order_id){
     if (isset($_SESSION['d97ld1in8osbcu'])) unset($_SESSION['d97ld1in8osbcu']);
