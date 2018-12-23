7e300b5ba222f110e84bc8bbd93b1dfd3de414d9
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-23 17:26:49 +0300

    12

--- inc/functions.php
+++ inc/functions.php
@@ -2,40 +2,41 @@
 session_start();
 $data = array('name' =>'','fam' =>'','number' =>'', 'email' =>'', 'product' =>'', 'price' =>'',);
 if(!empty($_POST)){
-    $data = load($data, $db);
-    $order_id = save('orders', $data);
-    setPaymentData($order_id);
+    $data = load($data);
+    save($db,'orders', $data);
+    //setPaymentData($order_id);
     header('Location: inc/form.php');
 }
 
-function setPaymentData($order_id){
-    if (isset($_SESSION['d97ld1in8osbcu'])) unset($_SESSION['d97ld1in8osbcu']);
-$_SESSION['d97ld1in8osbcu']['id']=$order_id;
-    $_SESSION['orders']['price']=$_POST['price'];
-};
+//function setPaymentData($order_id){
+//    if (isset($_SESSION['d97ld1in8osbcu'])) unset($_SESSION['d97ld1in8osbcu']);
+//$_SESSION['d97ld1in8osbcu']['id']=$order_id;
+//    $_SESSION['orders']['price']=$_POST['price'];
+//};
 
 /**
  * @param $data
  * @return mixed
  */
-function load ($data, $db){
-//    foreach ($_POST as $k => $v){
-//        if (array_key_exists($k, $data)){
-//            $data[$k] = $v;
-//        }
-//    }
-//    return $data;
-    $table_name="orders";
+function load ($data){
+    foreach ($_POST as $k => $v){
+        if (array_key_exists($k, $data)){
+            $data[$k] = $v;
+        }
+    }
+    return $data;
 
-    pg_copy_to($db, $table_name, $data);
+
+    //pg_copy_to($db, $table_name, $data);
 
 }
-function save ($table, $data){
-    $tbl = R::dispense($table);
-    foreach ($data as $k => $v){
-        $tbl->$k = $v;
-    }
- return R::store($tbl);
+function save ($db, $table, $data){
+//    $tbl = R::dispense($table);
+//    foreach ($data as $k => $v){
+//        $tbl->$k = $v;
+//    }
+// return R::store($tbl);
+    pg_copy_to($db, $table, $data);
 }
 function debug($data){
     echo '<pre>'. print_r($data, true).'</pre>';
