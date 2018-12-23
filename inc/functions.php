220bc965f34eea2bf97fb69fe47d26268d33e84b
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-23 22:33:52 +0300

    dffd

--- inc/functions.php
+++ inc/functions.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
 $data = array('name' =>'','fam' =>'','number' =>'', 'email' =>'', 'product' =>'', 'price' =>'',);
+
 if(!empty($_POST)){
     require_once 'db.php';
     $data = load($data);
-    save($db, $data);
-   // setPaymentData($order_id);
+    $order_id = save('orders', $data);
+    setPaymentData($order_id);
     header('Location: inc/form.php');
 }
+
 function setPaymentData($order_id){
     if (isset($_SESSION['d97ld1in8osbcu'])) unset($_SESSION['d97ld1in8osbcu']);
-$_SESSION['d97ld1in8osbcu']['id']=$order_id;
-    $_SESSION['orders']['price']=$_POST['price'];
+    $_SESSION['d97ld1in8osbcu']['id']=$order_id;
+    $_SESSION['d97ld1in8osbcu']['price']=$_POST['price'];
 };
+
 function load ($data){
     foreach ($_POST as $k => $v){
         if (array_key_exists($k, $data)){
@@ -21,20 +24,12 @@ function load ($data){
     }
     return $data;
 }
-
-/**
- * @param $db
- * @param $data
- */
-function save ($db, $data){
-    $query ="INSERT INTO orders (\"name\", \"fam\", \"number\", \"email\", \"product\", \"price\") VALUES ($data)";
-    pg_insert($db, 'orders',$data );
-    pg_close($db);
-//    $tbl = R::dispense($table);
-//    foreach ($data as $k => $v){
-//        $tbl->$k = $v;
-//    }
-// return R::store($tbl);
+function save ($table, $data){
+    $tbl = R::dispense($table);
+    foreach ($data as $k => $v){
+        $tbl->$k = $v;
+    }
+    return R::store($tbl);
 }
 function debug($data){
     echo '<pre>'. print_r($data, true).'</pre>';
