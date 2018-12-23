5e55ff394e32513da33eaf383e0a5025e5ad22a0
Alina Abramova <alinaabramova2016@yandex.ru>
2018-12-23 15:51:45 +0300

    12

--- inc/db.php
+++ inc/db.php
@@ -1,7 +1,7 @@
 <?php
-
+//
 //require_once 'rb.php';
-//$dbconn = pg_connect("dbname=d97ld1in8osbcu");
+////$dbconn = pg_connect("dbname=d97ld1in8osbcu");
 //$db = parse_url(getenv("postgres://mstptdpcpiqzsf:9eedaf81651c7f2ef284c3835908b4104ebe24e132dae104b4c593a1ae2fd7ce@ec2-54-247-125-116.eu-west-1.compute.amazonaws.com:5432/d97ld1in8osbcu
 //"));
 //$db["d97ld1in8osbcu"] = ltrim($db["d97ld1in8osbcu"], "/");
@@ -13,14 +13,14 @@
 //);
 //R::setup($db['dsn'], $db['user'],$db['pass']);
 //
-
-$pdo = new PDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-        $db["ec2-54-247-125-116.eu-west-1.compute.amazonaws.com"],
-        $db["5432"],
-        $db["mstptdpcpiqzsf"],
-        $db["9eedaf81651c7f2ef284c3835908b4104ebe24e132dae104b4c593a1ae2fd7ce"],
-        ltrim($db["d97ld1in8osbcu"], "/")
-    ));
+//
+//$pdo = new PDO("pgsql:" . sprintf(
+//        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+//        $db["ec2-54-247-125-116.eu-west-1.compute.amazonaws.com"],
+//        $db["5432"],
+//        $db["mstptdpcpiqzsf"],
+//        $db["9eedaf81651c7f2ef284c3835908b4104ebe24e132dae104b4c593a1ae2fd7ce"],
+//        ltrim($db["d97ld1in8osbcu"], "/")
+//    ));
 
 
--- inc/functions.php
+++ inc/functions.php
@@ -10,17 +10,29 @@ if(!empty($_POST)){
 }
 
 function setPaymentData($order_id){
-    if (isset($_SESSION['d97ld1in8osbcu'])) unset($_SESSION['payment']);
-$_SESSION['payment']['id']=$order_id;
-    $_SESSION['payment']['price']=$_POST['price'];
+    if (isset($_SESSION['d97ld1in8osbcu'])) unset($_SESSION['d97ld1in8osbcu']);
+$_SESSION['d97ld1in8osbcu']['id']=$order_id;
+    $_SESSION['orders']['price']=$_POST['price'];
 };
+
+/**
+ * @param $data
+ * @return mixed
+ */
 function load ($data){
-    foreach ($_POST as $k => $v){
-        if (array_key_exists($k, $data)){
-            $data[$k] = $v;
-        }
+//    foreach ($_POST as $k => $v){
+//        if (array_key_exists($k, $data)){
+//            $data[$k] = $v;
+//        }
+//    }
+//    return $data;
+    $db = pg_connect("host=ec2-54-247-125-116.eu-west-1.compute.amazonaws.com port=5432 dbname=d97ld1in8osbcu user=mstptdpcpiqzsf password=9eedaf81651c7f2ef284c3835908b4104ebe24e132dae104b4c593a1ae2fd7ce");
+    $table_name="orders";
+
+    pg_copy_to($db, $table_name, $data);
+    if ($db) {
+        echo "govno!";
     }
-    return $data;
 }
 function save ($table, $data){
     $tbl = R::dispense($table);
